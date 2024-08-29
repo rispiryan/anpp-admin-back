d433ba76056b53cc0e93f904561c7a9622849579
Levon <levon@nodoteam.com>
2024-08-29 21:42:38 +0400

    added auth module and jwt guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @ApiTags('Users')
 @Controller('users')
@@ -17,6 +18,7 @@ export class UsersController {
 
   @ApiOperation({ summary: 'Get users' })
   @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(JwtAuthGuard)
   @Get()
   getAll() {
     return this.userService.getAllUsers();
