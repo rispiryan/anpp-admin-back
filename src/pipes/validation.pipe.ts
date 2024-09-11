e3c0feaf902ba03c0586984d1a3b445580e2f56d
Levon <levon@nodoteam.com>
2024-09-12 00:35:02 +0400

    implemented cooperation table

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -7,10 +7,8 @@ import { ValidationException } from '../exeptions/validation.exception';
 export class ValidationPipe implements PipeTransform<any> {
   async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
     const obj = plainToClass(metadata.metatype, value);
-    console.log(value, 334334);
     const errors = await validate(obj);
     if (errors.length) {
-      console.log(errors, 999);
       const messages = errors.map((err) => {
         return `${err.property} - ${Object.values(err?.constraints).join(', ')}`;
       });
