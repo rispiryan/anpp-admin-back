8e005b6311f079d8395d2a675147e4d8a9c773b3
Levon <levon@nodoteam.com>
2024-09-12 01:19:56 +0400

    swager docs for cooperation

--- src/cooperation/cooperation.controller.ts
+++ src/cooperation/cooperation.controller.ts
@@ -8,12 +8,25 @@ import {
 import { CreateCooperationDto } from './dto/create-cooperation.dto';
 import { CooperationService } from './cooperation.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-
+import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+@ApiTags('Cooperation')
 @Controller('cooperation')
 export class CooperationController {
   constructor(private cooperationService: CooperationService) {}
-  @Post()
+  @Post('create')
   @UseInterceptors(FileInterceptor('image'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Create cooperation with image',
+    schema: {
+      type: 'object',
+      properties: {
+        title: { type: 'string' },
+        link: { type: 'string' },
+        image: { type: 'string', format: 'binary' },
+      },
+    },
+  })
   async createCooperation(
     @Body() dto: CreateCooperationDto,
     @UploadedFile() image,
