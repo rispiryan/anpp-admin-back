8a1731e31d509f6a46d2e0fd6fb4259c63a386f7
Razmik <razmik@Razmiks-MacBook-Pro.local>
2024-09-25 03:09:57 +0400

    feat: added temp cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,11 @@ async function start() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  app.enableCors({
+    origin: 'https://anpp-admin-back-production.up.railway.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Anpp project')
