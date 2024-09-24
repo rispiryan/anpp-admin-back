025ebefc536c30cbf6f2851429ce358b06e74f63
Razmik <razmik@Razmiks-MacBook-Pro.local>
2024-09-25 03:16:39 +0400

    feat: added temp cors

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,7 @@ async function start() {
   const app = await NestFactory.create(AppModule);
   console.log(process.env, 2323);
   app.enableCors({
-    origin: [
-      process.env.FRONT_DOMAIN,
-      'https://anpp-admin-back-production.up.railway.app',
-    ],
+    origin: [process.env.FRONT_DOMAIN, 'https://https://anpp-admin.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
