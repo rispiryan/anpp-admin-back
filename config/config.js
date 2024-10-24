67d4ac4a0e020ceca42bdb7e4d09ba186f249eef
Levon <levon@nodoteam.com>
2024-10-24 18:43:21 +0400

    test

--- config/config.js
+++ config/config.js
@@ -1,7 +1,17 @@
 const { resolve } = require('path');
 
-process.loadEnvFile(resolve('./.development.env'));
-
+process.loadEnvFile(resolve('./.production.env'));
+console.log(
+  {
+    dialect: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+  23323,
+);
 module.exports = {
   development: {
     dialect: 'postgres',
